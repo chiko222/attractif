179cb50b03c957cff8f79b7e9c25a3e49fbe4257
chiko222 <104754459+chiko222@users.noreply.github.com>
2022-07-01 10:46:39 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,66 +1,109 @@
-'use strict';
-
-{
-  //グローバルナビゲーション
-  const gnavOpen = document.querySelector('.hamburger-menu');
-  const overlay = document. querySelector('.overlay');
-  const links = overlay.querySelectorAll('a');
-
-  gnavOpen.addEventListener('click', () => {
-    gnavOpen.classList.toggle('active');
-    overlay.classList.toggle('show');
-  });
-
-  links.forEach(link => {
-    link.addEventListener('click', () => {
-      gnavOpen.classList.remove('active');
-      overlay.classList.remove('show');
-    });
-  });
-
-  //スクロールイベント
-  window.addEventListener('scroll', () => {
-    //スクロールに合わせてフェードイン
-    const fadeInTargets = document.querySelectorAll('.c-fade-in');
-    const scroll = window.scrollY;
-    const windowHeight = window.innerHeight;
-    fadeInTargets.forEach(target => {
-      const rect = target.getBoundingClientRect().top;     
-      const offset = rect + scroll;      
-      if(scroll > offset - windowHeight) {
-        if(target.classList.contains('c-fade-in--order')) {
-          target.style.opacity = '1';
-          const fadeUpOrder = Array.from(target.querySelectorAll('.c-fade-in-order-item'));
-          for(let i=0; i < fadeUpOrder.length; i++) {
-            const orderSp = String(i * 0.4);
-            const orderPc = String(i * 0.25);
-            if (window.matchMedia('(max-width:767px)').matches) {
-              fadeUpOrder[i].style.transitionDelay = orderSp + 's';
-            } else if (window.matchMedia('(min-width:768px)').matches) {
-              fadeUpOrder[i].style.transitionDelay = orderPc + 's';
-            }
-            fadeUpOrder[i].classList.add('show');
-          }
-        } else {
-          target.classList.add('show');
-        }
-      }
-    });
-
-    //ページトップへスクロール
-    const pageTop = document.querySelector('.page-top');
-    if(scroll > 1000) {
-      pageTop.classList.remove('down');
-      pageTop.classList.add('up');
-    } else {
-      pageTop.classList.remove('up');
-      pageTop.classList.add('down');
+//グローバルナビ
+const gnav = {
+  template: `
+    <div class="hamburgerMenu" @click="toggle">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <!-- オーバーレイ -->
+    <transition name="slide">
+    <div class="gnav" v-show="gnav">
+      <nav>
+        <ul>
+          <li class="gnav__item" v-for="item in items" :key="item"><a href="#">{{ item }}</a></li>
+        </ul>
+      </nav>
+      <ul class="gnav__snsLinks snsLinks">
+        <li v-for="icon in icons" :key="icon"><a href="" target="_blank" rel="noopener noreferrer"><i class="fa-brands" :class="icon"></i></a></li>
+      </ul>
+    </div>
+    </transition>
+  `,
+  data() {
+    return {
+      gnav: false,
+      items: [
+        'Top',
+        'Online Shop',
+        'Collection',
+        'Bridal',
+        'Concept',
+        'News',
+        'Contact'
+      ],
+      icons: [
+        'fa-twitter',
+        'fa-facebook-f',
+        'fa-instagram'
+      ]
     }
-    pageTop.addEventListener('click', () => {
-      window.scroll({
-        top: 0,
-        behavior: 'smooth'
-      });
-    });
-  });
+  },
+  methods: {
+    toggle() {
+      if(this.gnav) {
+        this.gnav = false;
+      } else {
+        this.gnav = true;
+      }
+      const toggle = document.querySelector('.hamburgerMenu');
+      toggle.classList.toggle('active');
+    }
+  }
 }
+
+Vue.createApp({
+  components: {
+    'gnav': gnav
+  }
+}).mount('#header');
+
+//商品情報
+const items =  [
+  {id: 1, img: 'img/pic-list-recommend01@2x.jpg', imgset: 'img/pic-list-recommend01.jpg 360w, img/pic-list-recommend01@2x.jpg 720w', alt: 'リング01', name: 'リング01',  price: '¥32,800', category: 'Ring', color: 'ゴールド、シルバー', size: '7号、9号、11号、13号、15号', quantity: 1},
+  {id: 2, img: 'img/pic-list-recommend02@2x.jpg', imgset: 'img/pic-list-recommend02.jpg 360w, img/pic-list-recommend02@2x.jpg 720w', alt: 'ピアス01', name: 'ピアス01', price: '¥24,000', category: 'Pierces', color: 'ゴールド', size: 'one size', quantity: 1},
+  {id: 3, img: 'img/pic-list-recommend03@2x.jpg', imgset: 'img/pic-list-recommend03.jpg 360w, img/pic-list-recommend03@2x.jpg 720w', alt: 'ヘアアクセサリー01', name: 'ヘアアクセサリー01', price: '¥14,500', category: 'Hair Accessory', color: 'ゴールド', size: 'one size', quantity: 1},
+  {id: 4, img: 'img/pic-list-recommend04@2x.jpg', imgset: 'img/pic-list-recommend04.jpg 360w, img/pic-list-recommend04@2x.jpg 720w', alt: 'ピアス02',  name: 'ピアス02',price: '¥21,500', category: 'Pierces', color: 'ゴールド', size: 'one size', quantity: 1},
+  {id: 5, img: 'img/pic-list-recommend05@2x.jpg', imgset: 'img/pic-list-recommend05.jpg 360w, img/pic-list-recommend05@2x.jpg 720w', alt: 'リング02', name: 'リング02', price: '¥38,400', category: 'Ring', color: 'ゴールド', size: '7号、9号、11号、13号、15号', quantity: 1},
+  {id: 6, img: 'img/pic-list-recommend06@2x.jpg', imgset: 'img/pic-list-recommend06.jpg 360w, img/pic-list-recommend06@2x.jpg 720w', alt: 'ピアス03', name: 'ピアス03', price: '¥19,000', category: 'Pierces', color: 'パール', size: 'one size', quantity: 1},
+  {id: 7, img: 'img/pic-list-recommend07@2x.jpg', imgset: 'img/pic-list-recommend07.jpg 360w, img/pic-list-recommend07@2x.jpg 720w', alt: 'ネックレス01', name: 'ネックレス01', price: '¥28,800', category: 'Necklace', color: 'シルバー', size: 'one size', quantity: 1},
+  {id: 8, img: 'img/pic-list-recommend08@2x.jpg', imgset: 'img/pic-list-recommend08.jpg 360w, img/pic-list-recommend08@2x.jpg 720w', alt: 'ネックレス02', name: 'ネックレス02', price: '¥20,500', category: 'Necklace', color: 'ゴールド', size: 'one size', quantity: 1},
+  {id: 9, img: 'img/pic-list-recommend09@2x.jpg', imgset: 'img/pic-list-recommend09.jpg 360w, img/pic-list-recommend09@2x.jpg 720w', alt: 'ピアス04', name: 'ピアス04', price: '¥10,500', category: 'Pierces', color: 'クリア', size: 'one size', quantity: 1},
+  {id: 10, img: 'img/pic-list-recommend10@2x.jpg', imgset: 'img/pic-list-recommend10.jpg 360w, img/pic-list-recommend10@2x.jpg 720w', alt: 'ネックレス03',  name: 'ネックレス03', price: '¥19,000', category: 'Necklace', color: 'ゴールド', size: 'one size', quantity: 1},
+]
+
+//Recommend
+const recommend = Vue.createApp({
+  data() {
+    return {
+      categoryOpen: false,
+      categories: [
+        'All', 'Necklace', 'Ring', 'Pierces', 'Hair Accessory'
+      ],
+      list: []
+    }
+  },
+  methods: {
+    toggle() {
+      if(this.categoryOpen) {
+        this.categoryOpen = false;
+      } else {
+        this.categoryOpen = true;
+      }
+      const toggle = document.querySelector('.category__toggle');
+      toggle.classList.toggle('active');
+    },
+    filter(event) {
+      if(event.target.dataset.filter === 'All'){
+        this.list =items.slice();
+      } else {
+        const filter = event.target.dataset.filter;
+        this.list = items.filter(item => item.category === filter);
+      }
+    }
+  },
+  mounted() {
+    this.list = items.slice();
+  }
+}).mount('#recommend')
+
--- js/top.js
+++ js/top.js
@@ -1,72 +1,207 @@
-'use strict';
-
-{
-  //loading
-  const loading = document.querySelector('.loading');
-  window.addEventListener('load', () => {
-    window.setTimeout (() => {
-      loading.classList.add('hide');
-    }, 1500);
-  });
-
-  //header slider
-  const slideImg = document.querySelectorAll('.top-header-img');
-  let count  = 0;
-  slideImg[count].classList.add('appear');
-  const slideChange = () => {
-    slideImg[count].classList.remove('appear');
-    count++;
-    if(count === slideImg.length) {
-      count = 0;
+//ローディング
+const loading = {
+  template: `  
+    <transition name="fadeSlow">
+      <div class="loading" v-show="load">
+        <transition name="fadeSlow" appear>
+          <p class="loading__logo">Attractif</p>
+        </transition>
+      </div>
+    </transition>
+  `,
+  data() {
+    return {
+      load: true
     }
-    slideImg[count].classList.add('appear');
-  }
-  setInterval(slideChange, 6000);
-
-  //modal window
-  const modal = document.querySelector('.modal-info');
-  const mask = document.querySelector('.mask');
-  window.addEventListener('load', () => {
+  },
+  mounted() {
     window.setTimeout(() => {
-      mask.classList.add('active');
-      modal.classList.add('show');
-    }, 4000);
-  });
-  const modalClose = document.querySelector('.modal-close');
-  modalClose.addEventListener('click', () => {
-    mask.classList.remove('active');
-    modal.classList.remove('show');
-  });
-
-  //swiper
-  window.addEventListener('load', () => {
-    const swiper = new Swiper(".swiper", {
-      loop: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false
-      },
-      slidesPerView: 1.5,
-      spaceBetween: 32,
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 64
-        },
-        992: {
-          slidesPerView: 3,
-          spaceBetween: 80
-
-        }
-      },
-      centeredSlides: true,
-      speed: 1500, 
-      allowTouchMove: false,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-      }
-    });
-  });
+      this.load = false;
+    }, 2500);
+  }
 }
+
+//モーダルウィンドウ
+const modal = {
+  template: `
+    <transition name="fade">
+      <div class="modal__wrapper" v-show="modal">
+        <section class="modal">
+          <div class="modal__close" @click="close">
+            <span></span>
+            <span></span>
+          </div>
+          <h3 class="modal__logo">ニュースレター登録</h3>
+          <p class="modal__text">最新情報やお得なご案内をいち早くお届けいたします。</p>
+          <a href="" class="modal__btn">登録</a>
+        </section>
+      </div>
+    </transition>
+  `,
+  data() {
+    return {
+      modal: false
+    }
+  },
+  methods: {
+    close() {
+      this.modal = false;
+    }
+  },
+  mounted() {
+    window.setTimeout(() => {
+      this.modal = true;
+    }, 6000);
+  }
+}
+
+Vue.createApp({
+  components: {
+    'loading': loading,
+    'modal': modal
+  }
+}).mount('#intro');
+
+//キービジュアル
+const kvSlide = {
+  template: `
+    <div class="kv__container">
+      <div class="kv__img" v-for="img in imgList" :key="img">        
+        <img :src="img.src" :srcset="img.srcset">
+      </div>        
+    </div>
+  `,
+  props: {
+    imgList: Array
+  },
+  methods: {
+    sort() {
+      //配列入れ替え
+      const firstImg = this.imgList[0];
+      this.imgList.shift();
+      this.imgList.push(firstImg);
+      //表示入れ替え
+      const newFirstImg = this.imgList[0].src;
+      const images = document.querySelectorAll('.kv__img');
+      images.forEach(image => {
+        const img = image.firstElementChild;
+        if(img.getAttribute('src') === newFirstImg) {
+          image.classList.add('show');
+        } else {
+          image.classList.remove('show');
+        }       
+      })
+    }
+  },
+  mounted() {
+    const images = document.querySelectorAll('.kv__img');
+    images[0].classList.add('show');
+    let timerId = setInterval(this.sort, 8000);
+  }
+}
+
+Vue.createApp({
+  template: `
+    <kv-slide :imgList="kvImgList"></kv-slide>
+  `,
+  components: {
+    'kv-slide': kvSlide
+  },
+  data() {
+    return {
+      kvImgList: [
+        {src: 'img/bg-top-kv01@2x.jpg', srcset: 'img/bg-top-kv01.jpg 1x, img/bg-top-kv01@2x.jpg 2x'},   
+        {src: 'img/bg-top-kv02@2x.jpg', srcset: 'img/bg-top-kv02.jpg 1x, img/bg-top-kv02@2x.jpg 2x'},   
+        {src: 'img/bg-top-kv03@2x.jpg', srcset: 'img/bg-top-kv03.jpg 1x, img/bg-top-kv03@2x.jpg 2x'}   
+      ]
+    }
+  }
+}).mount('#slider__apps');
+
+//Pick Up カルーセル
+window.addEventListener('load', () => {
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
+    },
+    slidesPerView: 1.5,
+    spaceBetween: 64,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 80
+
+      }
+    },
+    centeredSlides: true,
+    speed: 1500, 
+    allowTouchMove: false,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+});
+
+//スクロールに合わせてフェードイン
+window.addEventListener('scroll', () => {
+  const fadeInTargets = document.querySelectorAll('.fadeIn');
+  const scroll = window.scrollY;
+  const windowHeight = window.innerHeight;
+  fadeInTargets.forEach(target => {
+    const rect = target.getBoundingClientRect().top;     
+    const offset = rect + scroll;      
+    if(scroll > offset - windowHeight) {
+      if(target.classList.contains('fadeIn--order')) {
+        target.style.opacity = '1';
+        const fadeUpOrder = Array.from(target.querySelectorAll('.fadeIn--order'));
+        for(let i=0; i < fadeUpOrder.length; i++) {
+          const orderSp = String(i * 0.4);
+          const orderPc = String(i * 0.25);
+          if (window.matchMedia('(max-width:767px)').matches) {
+            fadeUpOrder[i].style.transitionDelay = orderSp + 's';
+          } else if (window.matchMedia('(min-width:768px)').matches) {
+            fadeUpOrder[i].style.transitionDelay = orderPc + 's';
+          }
+          fadeUpOrder[i].classList.add('show');
+        }
+      } else {
+        target.classList.add('show');
+      }
+    }
+  });
+});
+
+//ページトップへスクロール
+const scroll = Vue.createApp({
+  data() {
+    return {
+      arrive: false
+    }
+  },
+  methods: {
+    toggle() {
+      const scroll = window.scrollY;
+      if(scroll > 1000) {
+        this.arrive = true;
+      } else {
+        this.arrive = false;
+      }
+    },
+    scrollTop() {
+      window.scroll({
+        top: 0,
+        behavior: 'smooth'
+      });
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.toggle);
+  }
+}).mount('#scrollTop')
